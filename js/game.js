fe127ce8d334b6691741a71e9b52cfea265c807d
Miguel Rojas <leprosy57@gmail.com>
2014-12-01 17:57:19 -0300

    Cat can now destroy furniture!!

--- js/game.js
+++ js/game.js
@@ -1,165 +1,3 @@
-/* TUTORIALS
-//Works with crafty 0.6.2
-window.onload = function() {
-    //start crafty
-    Crafty.init(400, 320);
-
-    // Turn the sprite map into usable components
-    Crafty.sprite(16, "sprite.png", {
-        grass1: [0,0],
-        grass2: [1,0],
-        grass3: [2,0],
-        grass4: [3,0],
-        flower: [0,1],
-        bush1:  [0,2],
-        bush2:  [1,2],
-        player: [0,3]
-    });
-
-    // Method to ranCanvasy generate the map
-    function generateWorld() {
-        // Generate the grass along the x-axis
-        for (var i = 0; i < 25; i++) {
-                // Generate the grass along the y-axis
-                for (var j = 0; j < 20; j++) {
-                    grassType = Crafty.math.ranCanvasInt(1, 4);
-                    Crafty.e("2D, Canvas, grass" + grassType)
-                        .attr({x: i * 16, y: j * 16});
-
-                // 1/50 chance of drawing a flower and only within the bushes
-                if (i > 0 && i < 24 && j > 0 && j < 19 &&  Crafty.math.ranCanvasInt(0, 50) > 49) {
-                    Crafty.e("2D, Canvas, SpriteAnimation, flower")
-                        .attr({x: i * 16, y: j * 16})
-                        .reel("wind", 2000, 0, 1, 4)
-                        .animate("wind", -1);
-                }
-            }
-        }
-
-        // Create the bushes along the x-axis which will form the boundaries
-        for (var i = 0; i < 25; i++) {
-            Crafty.e("2D, Canvas, wall_top, bush"+ Crafty.math.ranCanvasInt(1,2))
-                .attr({x: i * 16, y: 0, z: 2});
-            Crafty.e("2D, Canvas, wall_bottom, bush"+ Crafty.math.ranCanvasInt(1,2))
-                .attr({x: i * 16, y: 304, z: 2});
-        }
-
-        // Create the bushes along the y-axis
-        // We need to start one more and one less to not overlap the previous bushes
-        for (var i = 1; i < 19; i++) {
-            Crafty.e("2D, Canvas, wall_left, bush" +  Crafty.math.ranCanvasInt(1,2))
-                .attr({x: 0, y: i * 16, z: 2});
-            Crafty.e("2D, Canvas, wall_right, bush" +  Crafty.math.ranCanvasInt(1,2))
-                .attr({x: 384, y: i * 16, z: 2});
-        }
-    }
-
-    Crafty.scene("main", function() {
-        generateWorld();
-
-        Crafty.c('CustomControls', {
-            __move: {left: false, right: false, up: false, down: false},    
-            _speed: 3,
-
-            CustomControls: function(speed) {
-                if (speed) this._speed = speed;
-                var move = this.__move;
-
-                this.bind('EnterFrame', function() {
-                    // Move the player in a direction depending on the booleans
-                    // Only move the player in one direction at a time (up/down/left/right)
-                    if (move.right) this.x += this._speed; 
-                    else if (move.left) this.x -= this._speed; 
-                    else if (move.up) this.y -= this._speed;
-                    else if (move.down) this.y += this._speed;
-                }).bind('KeyDown', function(e) {
-                    // Default movement booleans to false
-                    move.right = move.left = move.down = move.up = false;
-
-                    // If keys are down, set the direction
-                    if (e.key == Crafty.keys['RIGHT_ARROW']) move.right = true;
-                    if (e.key == Crafty.keys['LEFT_ARROW']) move.left = true;
-                    if (e.key == Crafty.keys['UP_ARROW']) move.up = true;
-                    if (e.key == Crafty.keys['DOWN_ARROW']) move.down = true;
-
-                    }).bind('KeyUp', function(e) {
-                    // If key is released, stop moving
-                    if (e.key == Crafty.keys['RIGHT_ARROW']) move.right = false;
-                    if (e.key == Crafty.keys['LEFT_ARROW']) move.left = false;
-                    if (e.key == Crafty.keys['UP_ARROW']) move.up = false;
-                    if (e.key == Crafty.keys['DOWN_ARROW']) move.down = false;
-                });
-
-                return this;
-            }
-        });
-
-        // Create our player entity with some premade components
-        var player = Crafty.e("2D, Canvas, player, Keyboard, CustomControls, SpriteAnimation, Collision")
-            .attr({x: 160, y: 144, z: 1})
-            .CustomControls(1)
-            .reel("walk_left", 500, 6, 3, 3)
-            .reel("walk_right", 500, 9, 3, 3)
-            .reel("walk_up", 500, 3, 3, 3)
-            .reel("walk_down", 500, 0, 3, 3)
-        .bind("EnterFrame", function(e) {
-            if (this.__move.left) {
-                if (!this.isPlaying("walk_left"))
-                    this.animate("walk_left", -1);
-            }
-            if (this.__move.right) {
-                if (!this.isPlaying("walk_right"))
-                    this.animate("walk_right", -1);
-            }
-            if (this.__move.up) {
-                if (!this.isPlaying("walk_up"))
-                    this.animate("walk_up", -1);
-            }
-            if (this.__move.down) {
-                if (!this.isPlaying("walk_down"))
-                    this.animate("walk_down", -1);
-            }
-        })
-        .bind("KeyUp", function(e) {
-            //this.stop();
-            this.pauseAnimation().resetAnimation();
-        })
-        .collision()
-        .onHit("wall_left", function() {
-            this.x += this._speed;
-            this.pauseAnimation();
-        }).onHit("wall_right", function() {
-            this.x -= this._speed;
-            this.pauseAnimation();
-        }).onHit("wall_bottom", function() {
-            this.y -= this._speed;
-            this.pauseAnimation();
-        }).onHit("wall_top", function() {
-            this.y += this._speed;
-            this.pauseAnimation();
-        });
-    });
-    
-    // The loading screen that will display while our assets load
-    Crafty.scene("loading", function() {
-        // Load takes an array of assets and a callback when complete
-        Crafty.load(["sprite.png"], function() {
-            
-            //Crafty.scene("main"); //when everything is loaded, run the main scene
-        });
-
-        // Black background with some loading text
-        Crafty.background("#000");
-        Crafty.e("2D, Canvas, Text").attr({w: 100, h: 20, x: 150, y: 120})
-            .text("Loading")
-            .textColor('#FFFFFF')
-            .css({"text-align": "center"});
-    });
-  
-    // Automatically play the loading scene
-    Crafty.scene("loading");
-}; */
-
 /* Global things */
 var Game = {};
 Game.tile = 32;
@@ -199,7 +37,7 @@ function serializeMap() {
 }
 
 
-
+/* Entry point */
 window.onload = function() {
     /* Start crafty */
     Crafty.init(Game.tile * 40, Game.tile * 20, 'game');
@@ -256,10 +94,11 @@ window.onload = function() {
     /* Scenes */
     // The loading screen
     Crafty.scene("loading", function() {
-        Crafty.load(["img/gato2.png", "img/sprite.png", "img/floor.png", "snd/elgato.mp3", "snd/miau.mp3"], function() {
+        Crafty.load(["img/gato2.png", "img/sprite.png", "img/floor.png", "snd/elgato.mp3", "snd/miau.mp3", "snd/rasgar.mp3"], function() {
             // Create resources (Using object for loading is more robust/efficient?)
             Crafty.audio.add("music", "snd/elgato.mp3");
             Crafty.audio.add("miau", "snd/miau.mp3");
+            Crafty.audio.add("rasgar", "snd/rasgar.mp3");
             Crafty.sprite(16, "img/floor.png", { piso: [0,0] });
             Crafty.sprite(Game.tile, "img/sprite.png", { cosas: [0,0] });
             Crafty.sprite(Game.tile, "img/gato2.png", { gato: [0,0] });
@@ -432,17 +271,29 @@ window.onload = function() {
 
                       var thing = e.hit('cosas');
                       var touchSomething = false;
-                      console.log(thing)
 
                       if (thing.length == 1) {
-                          console.log("Hay algo", thing[0].obj._itemId);
+                          var cosa = thing[0].obj; 
+                          cosa._catHits++;
+                          console.log("Hay algo", cosa);
 
-                          switch(thing[0].obj._itemId) {
+                          switch(cosa._itemId) {
                               case 15: // Sofa
                               case 6:
+                                  Crafty.audio.play("rasgar", 1);
                                   Game.score.sofa ++;
                                   console.log("Sofa", Game.score.sofa);
                                   touchSomething = true;
+
+                                  //Break the sofa
+                                  if (cosa._catHits == 5) {
+                                      var newId = cosa._itemId + 2;
+                                      var it = thingOff(newId);
+                                      cosa.sprite(it[0], it[1]);
+                                      cosa._itemId = newId;
+                                      
+                                  }
+
                                   break;
                               case 7: // Plant
                                   console.log("Plant");
@@ -463,7 +314,7 @@ window.onload = function() {
               });
 
         // Start!
-        Crafty.audio.play("music", -1);
+        //Crafty.audio.play("music", -1);
     });
 
 
